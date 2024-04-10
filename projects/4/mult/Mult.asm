dd2015280d63b362bbf619d5409e81ddb884fd60
Oscar Holt <oscar.holt99@gmail.com>
2024-04-11 11:55:15 +1200

    implementing Mult program

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,44 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// TODO Challenges:
+// Reset R2 to 0 at the beginning of the program
+// Make it more efficient by looping the fewer number of times
+// What if one of the values is 0?
+
+// Reset R2
+@R2
+M=0
+// Load R0 
+@R0
+D=M
+// i = R0 
+@i
+M=D
+// If i = 0, jump to END
+@END
+D;JEQ
+// Now we want to take the value of R1, and add it to itself
+(LOOP)
+@R1
+D=M
+// Add it to the sum
+@R2
+D=D+M
+M=D
+// Decrement i
+@i
+D=M-1
+M=D
+// If i=0, jump to END 
+@i
+D=M
+@END
+D;JEQ
+// Loop
+@LOOP
+0;JMP
+// End of program
+(END)
+@END
+0;JMP
